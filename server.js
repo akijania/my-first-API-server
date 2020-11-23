cc559dd672f9e5cf442a314e98a4a46b4bda8662
Anna Kijania <kijania.anna@gmail.com>
2020-11-23 13:53:54 +0100

    Init frontend project

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const db = require('./db');
+const cors = require('cors')
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', testimonialsRoutes);
-app.use('/', concertsRoutes);
-app.use('/', seatsRoutes);
+app.use('/api/', testimonialsRoutes);
+app.use('/api/', concertsRoutes);
+app.use('/api/', seatsRoutes);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
 });
 
-app.listen(7000, () => {
-  console.log('Server is running on port: 7000');
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
 });
