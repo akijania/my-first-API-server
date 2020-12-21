e35693b4501fce07cc3d0d722a2b6056f7f97fe7
Anna Kijania <kijania.anna@gmail.com>
2020-12-21 22:28:01 +0100

    Add joi

--- controllers/seats.controller.js
+++ controllers/seats.controller.js
@@ -1,5 +1,6 @@
 const Seat = require('../models/seat.model');
 const sanitize = require('mongo-sanitize');
+const Joi = require('joi');
 
 exports.getAll = async (req, res) => {
   try {
@@ -32,21 +33,31 @@ exports.getById = async (req, res) => {
 };
 
 exports.post = async (req, res) => {
+  const schema = Joi.object({
+    day: Joi.number().integer().min(1).max(3),
+    seat: Joi.number().integer().min(1).max(50),
+    client: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net', 'pl'] },
+    }),
+  });
   try {
     const { day, seat, client, email } = req.body;
-    if(!day || !seat|| !client|| !email) throw new Error('Invalid data');
+    if (!day || !seat || !client || !email) throw new Error('Invalid data');
     else {
-    const clientClean = sanitize(client);
-    const emailClean = sanitize(email);
-    const newSeat = new Seat({
-      day: day,
-      seat: seat,
-      client: clientClean,
-      email: emailClean,
-    });
-    await newSeat.save();
-    res.json({ message: 'OK' });
-  };
+      const value = await schema.validateAsync({ day: day, seat: seat, client: client, email: email });
+      const clientClean = sanitize(client);
+      const emailClean = sanitize(email);
+      const newSeat = new Seat({
+        day: day,
+        seat: seat,
+        client: clientClean,
+        email: emailClean,
+      });
+      await newSeat.save();
+      res.json({ message: 'OK' });
+    }
   } catch (err) {
     res.status(500).json({ message: "Couldn't connect to DB... Try again" });
   }
